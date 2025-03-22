eb987f53cc2de8c8f3b2af03e4b85876adc749f1
JEFFERSON CHAVEZ DIAZ <jefersonchavezdiaz.14@hotmail.com>
2025-03-22 17:54:53 -0500

    change window zoom level and font size to editor

---
+++ solid_principles/dependency_inversion.py
@@ -0,0 +1,3 @@
+"""
+The dependency inversion principle aims to reduce the coupling between classes by creating an abstraction layer between them.
+"""
---
+++ solid_principles/interface_segregation.py
@@ -0,0 +1,5 @@
+"""
+The interface segregation principle states that an interface should be as small a possible in terms of cohesion. In other words, it should do ONE thing.
+
+An interface is a description of behaviors that an object can do. For example, when you press the power button on the TV remote control, it turns the TV on, and you don’t need to care how.
+"""
