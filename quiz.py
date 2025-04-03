cedd7ffeca4f7ef841fb353450529427d4f804d4
JEFFERSON CHAVEZ DIAZ <jefersonchavezdiaz.14@hotmail.com>
2025-04-03 15:20:19 -0500

    add quiz

---
+++ quiz.py
@@ -0,0 +1,11 @@
+# Importando biblioteca math y utilizando la función pi
+from math import pi
+
+operation = [str(round(pi, i)) for i in range(0, 5)]
+print(operation)
+
+set1 = {50, 30, 70}
+set2 = {80, 50, 20}
+
+# Intersección
+print(set1 & set2)
