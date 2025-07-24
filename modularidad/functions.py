1db25cbc6426dfadf5fbd16f6c8a153797e3caeb
Jefferson <jefersonchavezdiaz.14@hotmail.com>
2025-07-24 14:18:51 -0500

    known about functions

---
+++ modularidad/functions.py
@@ -0,0 +1,11 @@
+""" FUNCTIONS"""
+"""
+A function is a named sequence of statements that performs a computation.
+"""
+
+# Function calls
+# Argument of the function
+# The result is also called the return value
+print(type(42))
+print(int('32'))
+
