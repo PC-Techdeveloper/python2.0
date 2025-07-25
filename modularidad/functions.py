8eb5f92c1abae88d4cfb98518a2ced23a34dd2f4
Jefferson <jefersonchavezdiaz.14@hotmail.com>
2025-07-24 19:46:46 -0500

    functions and new settings

--- modularidad/functions.py
+++ modularidad/functions.py
@@ -9,3 +9,7 @@ A function is a named sequence of statements that performs a computation.
 print(type(42))
 print(int('32'))
 
+# int can convert floating-point values to integers, but it doesn't
+# round off, it chops off the function part
+print(int(3.9999999))
+print(int(-2.3))
