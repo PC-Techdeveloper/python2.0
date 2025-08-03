aca4476302dbb8074074cf1a83f2c9f17d89714b
Jefferson <jefersonchavezdiaz.14@hotmail.com>
2025-08-03 11:59:20 -0500

    add new terminal and new file: iteration

---
+++ modularidad/iteration.py
@@ -0,0 +1,18 @@
+# Reassignment
+# A new reassignment makes an existing variable refer to a new value
+x = 5
+print(x)
+
+x = 7
+print(x)
+
+a = 5
+b = a
+a = 3
+print(b)
+
+# Updating variables
+x = 0
+x = x + 1
+
+print(x)
