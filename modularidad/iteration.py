aba7245bd6828e2f616f0a269ab686ce86575a86
Jefferson <jefersonchavezdiaz.14@hotmail.com>
2025-08-03 12:13:11 -0500

    square roots

--- modularidad/iteration.py
+++ modularidad/iteration.py
@@ -16,3 +16,40 @@ x = 0
 x = x + 1
 
 print(x)
+
+"""
+THE WHILE STATEMENT
+"""
+
+
+def countdown(n):
+    while n > 0:
+        print(n)
+        n = n - 1
+    print("Blastoff! 🚀")
+
+
+countdown(10)
+
+
+def sequence(n):
+    while n != 1:
+        print(n)
+        if n % 2 == 0:  # n is even
+            n = n / 2
+        else:
+            n = n * 3 + 1  # n is odd
+
+
+sequence(3)
+
+# break: Exits the loop
+while True:
+    line = input('Enter a word: ')
+    if line == 'done':
+        break
+    print(line)
+
+print('** Done **')
+
+""" SQUARE ROOTS"""
