2f703a00621a1b40f3d150b16e16f5e883fbab5c
Jefferson <jefersonchavezdiaz.14@hotmail.com>
2025-08-03 21:42:36 -0500

    string methods

---
+++ tipos_de_datos/string.py
@@ -0,0 +1,78 @@
+"""
+strings are not like integers, floats, and booleans. A string is a sequence, which means it is an ordered collection of other values.
+"""
+
+# A string is a sequence
+fruit = 'banana'
+letter = fruit[1]  # The expression in brackets is called an INDEX
+print(letter)
+
+# Accessing a character in a string
+letter = fruit[0]
+print(letter)
+
+# LEN: Len is a built-in function that returns the length of a string
+fruit = 'banana'
+print(len(fruit))
+
+# To get the last letter of a string
+length = len(fruit)
+last = fruit[length - 1]
+print(f"Last letter is: {last}")
+
+"""
+TRAVERSAL WITH A FOR LOOP
+"""
+print("Traversal with a for loop:")
+index = 0
+while index < len(fruit):
+    letter = fruit[index]
+    print(letter)
+    index = index + 1
+
+# FOR STATEMENTS
+print("For loop:")
+for letter in fruit:
+    print(letter)
+
+prefixes = 'JKLMNOPQ'
+suffix = 'ack'
+
+for letter in prefixes:
+    print(letter + suffix)
+
+"""
+STRING SLICES: A segment of a string is called a slice. Selecting a slice is similar to selecting a character.
+"""
+
+s = 'Monty Python'
+one = s[0:5]
+two = s[6:12]
+
+print(one)
+print(two)
+
+fruit = 'manzana'
+fruit = 'banana'
+print(fruit[0:3])
+print(fruit[:3])
+print(fruit[3:])
+print(fruit[:])
+print(fruit[3:3])
+
+"""
+STRING METHODS
+"""
+word = 'pera'
+new_word = word.upper()
+print(new_word)
+
+word = 'banana'
+index = word.find('a')
+indexSecond = word.find('na', 3)
+print(index)
+print(indexSecond)
+
+"""
+THE 'IN' OPERATOR
+"""
